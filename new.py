e47316c63ec13c21bd6ed80dda67f4ec04762ebf
unknown <kushalthapa1373@gmail.com>
2022-08-11 11:42:42 +0545

    lol

---
+++ new.py
@@ -0,0 +1,10 @@
+# ....> It represents a comment in python
+'''
+It represents a multi-line Comment
+We can write any thing here 
+'''
+
+a = 5 #here, a is a variable which stores/holds integer typed value i.e 5
+name - "name" #here, name is a varibale which stores/holds string type value i.e "name"
+#And in python string are always iside single or double quotes:
+# for example: "name" OR 'name'
