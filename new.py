d49debe0a8a0be8e22023f9fe8cfe27db8c29c16
unknown <kushalthapa1373@gmail.com>
2022-08-16 12:01:38 +0545

    demo about function

--- new.py
+++ new.py
@@ -6,5 +6,36 @@ We can write any thing here
 
 a = 5 #here, a is a variable which stores/holds integer typed value i.e 5
 name - "name" #here, name is a varibale which stores/holds string type value i.e "name"
-#And in python string are always iside single or double quotes:
+#And in python string are always inside single or double quotes:
 # for example: "name" OR 'name'
+
+'''
+
+syntax :
+
+#creating function 
+def function_name():
+    #you can do anything here 
+    return ...... something 
+
+    #calling function 
+    function_name()
+
+    ....
+
+    
+    #example
+
+    #creating function 
+    def duita_num_jodney():
+        sum-5+7
+        print(sum)
+        return sum
+
+        #calling function with their name 
+        duita_num_joda()
+
+        #taking input from user 
+       my_name=input("please input your name-->: ")
+       print(my_name)
+'''
